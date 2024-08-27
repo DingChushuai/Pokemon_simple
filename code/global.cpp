2dafce6537ca4fd861c3196f1ef6e319fdd1d8a0
德义 刘 <sdta.liudy@foxmail.com>
2024-08-27 09:31:01 +0800

    global中两个函数

--- code/global.cpp
+++ code/global.cpp
@@ -1,13 +1,23 @@
 #include "global.h"
+#include<sstream>
 
 inline vector<string> split(const string& s, char delimiter)
 {
     //根据给定的分隔符，将字符串分割成多个子字符串，并返回一个字符串向量
-    return vector<string>();
+    vector<string>result;
+    stringstream ss(s);
+    string temp;
+    while (ss, temp, delimiter) {
+        result.push_back(temp);
+    }
+    return result;
 }
 
 inline int stoi(const string& s)
 {
     //将字符串转换为整数
-    return 0;
+    istringstream is(s);
+    int num;
+    is >> num;
+    return num;
 }
